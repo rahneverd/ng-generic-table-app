ce5f18c7b5ab48435785c19dcc81206a45f5271c
rahneverd <rahneverd@gmail.com>
2022-12-28 21:29:09 +0500

    validation added and dynamic form generation implemented

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  keyObj = {
+    first_name : ['', [Validators.required, Validators.minLength(4)]],
+    last_name: ['', [Validators.required, Validators.minLength(4)]],
+    age: [, [Validators.min(2), Validators.required]],
+    languages: ['', [Validators.required, Validators.minLength(3)]]
+  }
   keyArray= [
-    {key: "first_name", name: "First Name"},
-    {key: "last_name", name: "Last Name"},
-    {key: "age", name: "Age"},
-    {key: "angular", name: "Angular"}
+    {key: "first_name", name: "First Name", type: "string"},
+    {key: "last_name", name: "Last Name", type: "string"},
+    {key: "age", name: "Age", type: "number"},
+    {key: "languages", name: "Languages", type: "string"}
   ]
   dataArray= [
-    {first_name: "Muhammad", last_name: "Owais", age: 27, angular: "JS"},
-    {first_name: "Muhammad", last_name: "Owais", age: 27, angular: "JS"},
-    {first_name: "Muhammad", last_name: "Owais", age: 27, angular: "JS"},
-    {first_name: "Muhammad", last_name: "Owais", age: 27, angular: "JS"},
-    {first_name: "Muhammad", last_name: "Owais", age: 27, angular: "JS"},
+    {first_name: "Muhammad", last_name: "Owais", age: 27, languages: "Angular JS"},
+    {last_name: "Owais", first_name: "Muhammad", age: 27, languages: "Node JS"},
+    {first_name: "Muhammad", age: 27, languages: "Vue JS", last_name: "Owais"},
+    {languages: "React JS", first_name: "Muhammad", last_name: "Owais", age: 27},
+    {first_name: "Muhammad", last_name: "Owais", age: 27, languages: "Nest JS"},
   ]
   constructor() { }
 }
--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-input-form',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-form.component.css']
 })
 export class InputFormComponent implements OnInit {
-
-  constructor() { }
+  keyArray
+  constructor(private _data: DataService, private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.keyArray = this._data.keyArray
+    console.log(this.dataForm)
   }
+
+  dataForm = this._fb.group(this._data.keyObj)
+
 
 }
