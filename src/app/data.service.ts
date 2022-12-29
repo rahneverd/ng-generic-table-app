a4f8ced05aac1269985d1825da37b9d0fac2977b
rahneverd <rahneverd@gmail.com>
2022-12-29 14:50:47 +0500

    completed

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,20 +9,22 @@ export class DataService {
     first_name : ['', [Validators.required, Validators.minLength(4)]],
     last_name: ['', [Validators.required, Validators.minLength(4)]],
     age: [, [Validators.min(2), Validators.required]],
-    languages: ['', [Validators.required, Validators.minLength(3)]]
+    languages: ['', [Validators.required, Validators.minLength(3)]],
+    city: ['', Validators.required]
   }
   keyArray= [
-    {key: "first_name", name: "First Name", type: "string"},
-    {key: "last_name", name: "Last Name", type: "string"},
+    {key: "first_name", name: "First Name", type: "text"},
+    {key: "last_name", name: "Last Name", type: "text"},
     {key: "age", name: "Age", type: "number"},
-    {key: "languages", name: "Languages", type: "string"}
+    {key: "languages", name: "Languages", type: "text"},
+    {key: "city", name: "City", type: "password"}
   ]
   dataArray= [
-    {first_name: "Muhammad", last_name: "Owais", age: 27, languages: "Angular JS"},
-    {last_name: "Owais", first_name: "Muhammad", age: 27, languages: "Node JS"},
-    {first_name: "Muhammad", age: 27, languages: "Vue JS", last_name: "Owais"},
-    {languages: "React JS", first_name: "Muhammad", last_name: "Owais", age: 27},
-    {first_name: "Muhammad", last_name: "Owais", age: 27, languages: "Nest JS"},
+    {city: "Rawalpindi", first_name: "Muhammad", last_name: "Owais", age: 27, languages: "Angular JS"},
+    {last_name: "Owais", city: "Rawalpindi", first_name: "Muhammad", age: 27, languages: "Node JS"},
+    {first_name: "Muhammad", age: 27, languages: "Vue JS",city: "Rawalpindi", last_name: "Owais"},
+    {languages: "React JS", first_name: "Muhammad", last_name: "Owais", age: 27,city: "Rawalpindi"},
+    {first_name: "Muhammad",city: "Rawalpindi", last_name: "Owais", age: 27, languages: "Nest JS"},
   ]
   constructor() { }
 }
